b449e5b3429505d6507fe93ac073656a4a2b2e19
IU3 Student <IU3 Student>
2016-11-15 14:31:27 +0300

    Add code from lab_1 to lab_2

--- WTIS_Lab_2/main.cpp
+++ WTIS_Lab_2/main.cpp
@@ -7,9 +7,17 @@
 
 // Process types
 typedef OS::process<OS::pr0, 500> PROCESS_MAIN;
+typedef OS::process<OS::pr1, 200> PROCESS_READ;
 
 // Process objects
 static PROCESS_MAIN Process_Main;
+static PROCESS_READ Process_Read;
+
+//Передача сообщений между процессами
+OS::message<float> temp_message;
+
+//флаг событий
+OS::TEventFlag flag;
 
 size_t StackSlacks[OS::PROCESS_COUNT];
 
@@ -28,6 +36,14 @@ static unsigned char PacketData[CC2420_FRAME_PAYLOAD_MAX_SIZE];
 static int PacketDataSize;
 
 void CC2420_OnReceivedPacket(RADIO_PACKET_RX_INFO &Info, unsigned char *Data, unsigned char Size);
+
+#define T_Sample 64
+#define T_Send 256
+#define T_Treshold 0.1
+// коэффициент сглаживания
+#define alpha 0.7 
+
+float sended_temp;
 
 void main()
 {
@@ -61,16 +77,70 @@ void OS::IdleProcessUserHook()
 }
 //---------------------------------------------------------------------------
 
-template<> void PROCESS_MAIN::exec()
+
+//Симуляция воздействия ФНЧ на серию цифровых отсчетов
+float Filter(float last_temp, float temp) {
+
+  return alpha * temp + (1 - alpha) * last_temp;
+
+}
+
+
+template<> void PROCESS_READ::exec()
 {
+  float current_temp;
+  float prev_temp;
+  float filtered_temp;
+  
+  prev_temp = Sensors_IntTemperature_Read();
+  
   LED_1_On();
   LED_2_Off();
+  
   for (;;)
   {
     // Тело основного процесса
+    current_temp = Sensors_IntTemperature_Read();
+    filtered_temp = Filter(prev_temp, current_temp);
+    
+    temp_message = filtered_temp;
+    
+    //если температура по модулю больше чем в константе
+    if (abs(sended_temp - filtered_temp) > T_Treshold) {
+      //сигналим о изменениях
+      flag.signal();
+    }
+    
+    //отправка отфильтрованных данных
+    temp_message.send();
+      
+    prev_temp = filtered_temp;
+    
     LED_1_Toggle();
     LED_2_Toggle();
-    sleep(64);
+    
+    sleep(T_Sample);
+   
+  }
+}
+//---------------------------------------------------------------------------
+
+template<> void PROCESS_MAIN::exec()
+{
+  float temp;
+  
+  for (;;)
+  {
+    if (flag.wait(T_Send)) {
+      temp_message.out(temp);
+      printf("Alarm out temp=%2.2f degrees celsius\n\r", temp);
+      sended_temp = temp;
+      
+    } else {
+      temp_message.out(temp);
+      printf("Out temp=%2.2f degrees celsius\n\r", temp);
+      sended_temp = temp;
+    }
   }
 }
 //---------------------------------------------------------------------------
