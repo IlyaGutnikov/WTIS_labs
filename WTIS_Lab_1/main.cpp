116cbef8e647a3274ebc6fff7a6a6214c831b164
IU3 Student <IU3 Student>
2016-11-15 13:08:30 +0300

    Finished lab_1

--- WTIS_Lab_1/main.cpp
+++ WTIS_Lab_1/main.cpp
@@ -6,9 +6,17 @@
 
 // Process types
 typedef OS::process<OS::pr0, 500> PROCESS_MAIN;
+typedef OS::process<OS::pr1, 500> PROCESS_READ;
 
 // Process objects
 static PROCESS_MAIN Process_Main;
+static PROCESS_READ Process_Read;
+
+//Передача сообщений между процессами
+OS::message<float> temp_message;
+
+//флаг событий
+OS::TEventFlag flag;
 
 size_t StackSlacks[OS::PROCESS_COUNT];
 
@@ -26,6 +34,8 @@ size_t StackSlacks[OS::PROCESS_COUNT];
 #define T_Treshold 0.1
 // коэффициент сглаживания
 #define alpha 0.7 
+
+float sended_temp;
 
 void main()
 {
@@ -61,26 +71,62 @@ float Filter(float last_temp, float temp) {
 
 }
 
-template<> void PROCESS_MAIN::exec()
+
+template<> void PROCESS_READ::exec()
 {
   float current_temp;
   float prev_temp;
   float filtered_temp;
   
+  prev_temp = Sensors_IntTemperature_Read();
+  
   LED_1_On();
   LED_2_Off();
+  
   for (;;)
   {
     // Тело основного процесса
     current_temp = Sensors_IntTemperature_Read();
     filtered_temp = Filter(prev_temp, current_temp);
-    printf("Temperature=%2.2f degrees Celsius\n\r", filtered_temp);
+    
+    temp_message = filtered_temp;
+    
+    //если температура по модулю больше чем в константе
+    if (abs(sended_temp - filtered_temp) > T_Treshold) {
+      //сигналим о изменениях
+      flag.signal();
+    }
+    
+    //отправка отфильтрованных данных
+    temp_message.send();
+      
+    prev_temp = filtered_temp;
+    
     LED_1_Toggle();
     LED_2_Toggle();
-    sleep(T_Send);
     
-    prev_temp = filtered_temp;
+    sleep(T_Sample);
+   
   }
 }
 //---------------------------------------------------------------------------
 
+
+template<> void PROCESS_MAIN::exec()
+{
+  float temp;
+  
+  for (;;)
+  {
+    if (flag.wait(T_Send)) {
+      temp_message.out(temp);
+      printf("Alarm out temp=%2.2f degrees celsius\n\r", temp);
+      sended_temp = temp;
+      
+    } else {
+      temp_message.out(temp);
+      printf("Out temp=%2.2f degrees celsius\n\r", temp);
+      sended_temp = temp;
+    }
+  }
+}
